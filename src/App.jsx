2960d0e50d05ff311294d13ebce827d1409c841a
Your Name <dan.weibren@gmail.com>
2025-08-27 14:15:03 +0300

    feat(router): add dynamic router selection for gh-pages
    - Import both BrowserRouter and HashRouter
    - Select router based on environment variables
    - Ensure compatibility with GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes, HashRouter } from 'react-router-dom'
 import { Provider } from "react-redux"
 
 import { ToyIndex } from "./pages/ToyIndex.jsx"
@@ -9,28 +9,34 @@ import { ToyEdit } from "./pages/ToyEdit.jsx"
 import './assets/style/main.css'
 import { store } from './store/store'
 import { UserMsg } from './cmps/UserMsg.jsx'
+
+const Router =
+  import.meta.env.MODE === "production" && import.meta.env.VITE_DEPLOY_TARGET === "gh-pages"
+    ? HashRouter
+    : BrowserRouter
+
 function App() {
 
-  return (
-    <>
-        <Provider store={store} >
-            <Router>
-                <section className="app main-layout">
-                <UserMsg/>
-                    <main>
-                        <Routes>
-                            <Route path="/" element={<ToyIndex />} />
-                            <Route path="/toy/:toyId" element={<ToyDetails />} />
-                            <Route path="/toy/edit/:toyId" element={<ToyEdit />} />
-                            <Route path="/toy/edit" element={<ToyEdit />} />
-                            <Route path="/toy" element={<ToyIndex />} />
-                        </Routes>
-                    </main>
-                </section>
-            </Router>
-        </Provider>
-    </>
-  )
+    return (
+        <>
+            <Provider store={store} >
+                <Router>
+                    <section className="app main-layout">
+                        <UserMsg />
+                        <main>
+                            <Routes>
+                                <Route path="/" element={<ToyIndex />} />
+                                <Route path="/toy/:toyId" element={<ToyDetails />} />
+                                <Route path="/toy/edit/:toyId" element={<ToyEdit />} />
+                                <Route path="/toy/edit" element={<ToyEdit />} />
+                                <Route path="/toy" element={<ToyIndex />} />
+                            </Routes>
+                        </main>
+                    </section>
+                </Router>
+            </Provider>
+        </>
+    )
 }
 
 export default App
