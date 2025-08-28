3d9b836448bd718ec794d98061b09f4caf78ed7b
Your Name <dan.weibren@gmail.com>
2025-08-28 09:44:02 +0300

    feat(labels): add custom update-only hook
    - introduce useEffectOnUpdateOnly to skip first render
    - refactor Labels.jsx to use new hook for label updates
    - simplify label addition logic

---
+++ src/hooks/useEffectOnUpdateOnly.js
@@ -0,0 +1,17 @@
+import { useEffect, useRef } from "react";
+
+
+export function useEffectOnUpdate(func, dependencies) {
+
+    const isItFirstRender = useRef(true)
+
+    useEffect(() => {
+        if (isItFirstRender.current) {
+            isItFirstRender.current = false
+        } else {
+            func()
+        }
+    }, dependencies)
+
+
+}
