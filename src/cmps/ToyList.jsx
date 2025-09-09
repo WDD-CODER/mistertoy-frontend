76cd1e82dd12d21c42939a77f785f7a31f40f4f0
Your Name <dan.weibren@gmail.com>
2025-09-09 11:31:54 +0300

    refactor(toys): remove onToggleInStock prop and usage from ToyList
    - Remove onToggleInStock prop in ToyList component
    - Remove related button functionality from ToyList
    - Update ToyPreview usage without onToggleInStock

--- src/cmps/ToyList.jsx
+++ src/cmps/ToyList.jsx
@@ -3,7 +3,7 @@ import { LabelsList } from "./LabelsList.jsx"
 import { ToyPreview } from "./ToyPreview.jsx"
 import { Link } from "react-router-dom"
 
-export function ToyList({ toys, onRemoveToy, onToggleInStock }) {
+export function ToyList({ toys, onRemoveToy }) {
 
     //לא מובן מדוע אם אין את הקונסול בפרטים אז לפעמים צריך כמה לחיצות עד שהכפתור מגיב
     
@@ -11,7 +11,7 @@ export function ToyList({ toys, onRemoveToy, onToggleInStock }) {
         <List className="toy-list" >
             {toys.map(toy =>
                 <Card key={toy._id}  sx={{ height: 'auto', display: 'flex', flexDirection: 'column',  }}>
-                        <ToyPreview toy={toy} onToggleInStock={onToggleInStock} />
+                        <ToyPreview toy={toy}/>
                         <LabelsList toy={toy} />
                         <ButtonGroup>
                             <Button onClick={() => onRemoveToy(toy._id)}>Remove</Button>
