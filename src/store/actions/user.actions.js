a9a7d05cc25efdf63ca48ef3e0d4bb947d041691
Your Name <dan.weibren@gmail.com>
2025-09-09 12:24:16 +0300

    refactor(actions): convert loginUser and signupUser to async/await
    - Use async/await in user action creators
    - Improve error handling and dispatch user data correctly

--- src/store/actions/user.actions.js
+++ src/store/actions/user.actions.js
@@ -3,22 +3,29 @@ import { userService } from "../../services/user.service.js";
 import { SET_CREDENTIALS, SET_LOGGED_IN_USER } from "../reduce/user.reduce.js";
 import { store } from "../store.js";
 
-export function loginUser(credentials) {
-    return userService.login(credentials)
-        .then(user => store.dispatch({ type: SET_LOGGED_IN_USER, user }))
+export async function loginUser(credentials) {
+    try {
+        const userDetails = await userService.login(credentials)
+        store.dispatch({ type: SET_LOGGED_IN_USER, user })
+    } catch (err) {
+        console.log("user.actions -> can't login", err)
+        throw err
+    }
 }
 
-export function signupUser(credentials) {
-    return userService.query()
-        .then(users => users.find(user => user.username === credentials.username))
-        .then(user => {
-            if (!user) {
-                store.dispatch({ type: SET_CREDENTIALS, credentials })
-                userService.signup(credentials)
-                    .then(user => store.dispatch({ type: SET_LOGGED_IN_USER, user }))
-            }
-            else return Promise.reject(' Username exist already')
-        })
+export async function signupUser(credentials) {
+    try {
+        const users = await userService.query()
+        const user = users.find(user => user.username === credentials.username)
+        if (!user) {
+            store.dispatch({ type: SET_CREDENTIALS, credentials })
+            const userCredentials = await userService.signup(credentials)
+            store.dispatch({ type: SET_LOGGED_IN_USER, user })
+        }
+        else return Promise.reject(' Username exist already')
+    } catch (err) {
+        console.log("user.actions -> can't login", err)
+    }
 }
 
 
